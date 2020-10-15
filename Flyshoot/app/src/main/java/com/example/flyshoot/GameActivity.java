e4dfd1b577166f7168bc6e09cd1958b81863bcb4
maskur15 <sabilhasan2018@gmail.com>
2020-10-15 19:26:31 +0600

    All python file added

---
+++ Flyshoot/app/src/main/java/com/example/flyshoot/GameActivity.java
@@ -0,0 +1,30 @@
+package com.example.flyshoot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class GameActivity extends AppCompatActivity {
+    private  GameView gameView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+        gameView = new GameView(this,point.x,point.y);
+        setContentView(gameView);
+    }
+   protected void onPause()
+   {
+       super.onPause();
+       gameView.pause();
+   }
+   protected  void onResume()
+   {
+       super.onResume();
+       gameView.resume();
+   }
+}
